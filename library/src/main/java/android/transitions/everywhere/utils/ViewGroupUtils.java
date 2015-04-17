1dfe185f44693abeb9ba60654542ac1265e37476
Andrey Kulikov <and.kulikov@gmail.com>
2015-04-17 17:19:03 +0300

    backup and restore previous LayoutTransition in suppressLayout

--- library/src/main/java/android/transitions/everywhere/utils/ViewGroupUtils.java
+++ library/src/main/java/android/transitions/everywhere/utils/ViewGroupUtils.java
@@ -2,14 +2,30 @@ package android.transitions.everywhere.utils;
 
 import android.animation.LayoutTransition;
 import android.annotation.TargetApi;
-import android.os.Build.VERSION;
+import android.os.Build;
 import android.os.Build.VERSION_CODES;
+import android.transitions.everywhere.R;
 import android.view.ViewGroup;
 
 import java.lang.reflect.Field;
 
 @TargetApi(VERSION_CODES.HONEYCOMB)
 public class ViewGroupUtils {
+
+    public static final LayoutTransition EMPTY_LAYOUT_TRANSITION = new LayoutTransition() {
+        @Override
+        public boolean isChangingLayout() {
+            return true;
+        }
+    };
+    static {
+        EMPTY_LAYOUT_TRANSITION.setAnimator(LayoutTransition.APPEARING, null);
+        EMPTY_LAYOUT_TRANSITION.setAnimator(LayoutTransition.CHANGE_APPEARING, null);
+        EMPTY_LAYOUT_TRANSITION.setAnimator(LayoutTransition.CHANGE_DISAPPEARING, null);
+        EMPTY_LAYOUT_TRANSITION.setAnimator(LayoutTransition.DISAPPEARING, null);
+        EMPTY_LAYOUT_TRANSITION.setAnimator(LayoutTransition.CHANGING, null);
+    }
+
     interface ViewGroupUtilsImpl {
         void suppressLayout(ViewGroup group, boolean suppress);
     }
@@ -19,26 +35,15 @@ public class ViewGroupUtils {
 
         private static Field sFieldLayoutSuppressed;
 
-        static LayoutTransition mLayoutTransition = new LayoutTransition() {
-            @Override
-            public boolean isChangingLayout() {
-                return true;
-            }
-        };
-        static {
-            mLayoutTransition.setAnimator(LayoutTransition.APPEARING, null);
-            mLayoutTransition.setAnimator(LayoutTransition.CHANGE_APPEARING, null);
-            mLayoutTransition.setAnimator(LayoutTransition.CHANGE_DISAPPEARING, null);
-            mLayoutTransition.setAnimator(LayoutTransition.DISAPPEARING, null);
-            mLayoutTransition.setAnimator(LayoutTransition.CHANGING, null);
-        }
-
         @Override
         public void suppressLayout(ViewGroup group, boolean suppress) {
             if (suppress) {
-                group.setLayoutTransition(mLayoutTransition);
+                if (group.getLayoutTransition() != null) {
+                    group.setTag(R.id.group_layouttransition_cache, group.getLayoutTransition());
+                }
+                group.setLayoutTransition(EMPTY_LAYOUT_TRANSITION);
             } else {
-                group.setLayoutTransition(null);
+                group.setLayoutTransition((LayoutTransition) group.getTag(R.id.group_layouttransition_cache));
                 if (sFieldLayoutSuppressed == null) {
                     sFieldLayoutSuppressed = ReflectionUtils.getPrivateField(ViewGroup.class,
                             "mLayoutSuppressed");
@@ -64,7 +69,7 @@ public class ViewGroupUtils {
     private static final ViewGroupUtilsImpl IMPL;
 
     static {
-        if (VERSION.SDK_INT >= VERSION_CODES.JELLY_BEAN_MR2) {
+        if (Build.VERSION.SDK_INT >= VERSION_CODES.JELLY_BEAN_MR2) {
             IMPL = new JellyBeanMr2ViewGroupUtilsImpl();
         } else {
             IMPL = new BaseViewGroupUtilsImpl();
