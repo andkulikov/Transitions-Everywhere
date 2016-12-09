4b68830f08f1117cc0e928bf3806f860cdff2b5f
Andrey Kulikov <andkulikov@gmail.com>
2016-12-09 00:12:14 +0000

    add missed copyrights

--- sample/src/main/java/com/andkulikov/transitionseverywhere/RotateSample.java
+++ sample/src/main/java/com/andkulikov/transitionseverywhere/RotateSample.java
@@ -1,7 +1,20 @@
+/*
+ * Copyright (C) 2016 Andrey Kulikov (andkulikov@gmail.com)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.andkulikov.transitionseverywhere;
 
-import android.annotation.TargetApi;
-import android.os.Build;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
@@ -9,8 +22,8 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-import com.transitionseverywhere.TransitionManager;
 import com.transitionseverywhere.Rotate;
+import com.transitionseverywhere.TransitionManager;
 
 /**
  * Created by Andrey Kulikov on 17/04/16.
